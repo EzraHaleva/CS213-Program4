eb77e9ae0e08b3169bdcb3468df46a559f1a0d71
Robert <robert.bonagura@gmail.com>
2020-04-08 08:12:24 -0400

    Add documentation

--- src/BackEnd/PizzaOrder.java
+++ src/BackEnd/PizzaOrder.java
@@ -18,18 +18,34 @@ import java.util.ArrayList;
    -Ezra
 
  */
+
+/**
+ * Class used to represent all information associated with a single pizza
+ * order.
+ */
 public class PizzaOrder {
     static ArrayList<Pizza> pizzaList;
 
+    /**
+     * Initializes pizzalist to empty.
+     */
     public PizzaOrder(){
-
         this.pizzaList = new ArrayList<Pizza>();
     }
+
+    /**
+     * Seems redundant, as it does the same things as default constructor.
+     */
     public static void initialize(){
         pizzaList = new ArrayList<Pizza>();
     }
 
-
+    /**
+     * Creates specifies pizza and adds to pizza order list.
+     * @param style Specifies what style of pizza
+     * @param size  Specifies size of pizza
+     * @param toppings  A list of toppings on pizza
+     */
     public static void addPizzaToOrder(String style, String size, ArrayList<String> toppings){
         if(style.equals("Build Your Own")){
             BuildYourOwn pizzaToAdd = new BuildYourOwn(size, toppings);
@@ -42,10 +58,20 @@ public class PizzaOrder {
             pizzaList.add(pizzaToAdd);
         }
     }
+
+    /**
+     * Clears the pizza order by assigning a new list of of pizzas to the
+     * pizzaList variable.
+     */
     public static void clearOrder(){
         pizzaList = new ArrayList<Pizza>();
     }
 
+    /**
+     * Returns the total cost of order by summing up the cost of each pizza
+     * in list.
+     * @return Total cost of order.
+     */
     public static int orderTotal(){
         int orderTotal = 0;
         for(Pizza pizza : pizzaList){
@@ -53,14 +79,20 @@ public class PizzaOrder {
         }
         return orderTotal;
     }
+
+    /**
+     * Returns details of pizzaList as a String.
+     * @return String detail of pizzaList.
+     */
     public static String details(){
-        String output = "";
+        StringBuilder output = new StringBuilder();
         for(Pizza pizza : pizzaList){
-            output += pizza.toString();
+            output.append(pizza.toString());
         }
         int orderTotal = orderTotal();
-        output += ("Total cost of order: $" + orderTotal + "\n");
-        return output;
+        output.append("Total cost of order: $");
+        output.append(orderTotal).append("\n");
+        return output.toString();
     }
 
 
--- src/FrontEnd/Main.java
+++ src/FrontEnd/Main.java
@@ -7,8 +7,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main class to run Pizza Store application.
+ */
 public class Main extends Application {
 
+    /**
+     * Creates stage for Pizza Store app.
+     * Initialize the PizzaOrder.
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("OrderScreenStage.fxml"));
